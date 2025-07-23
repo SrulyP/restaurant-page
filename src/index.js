23dcf9a86e3390414c9361f5d7295e1dfb7c5138
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-07-23 14:02:01 +0300

    Start app object for modularization of the index page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const App = {
+    init() {
+        this.cacheDom();
+        this.render();
+        this.appending();
+    },
+
+    cacheDom() {
+        this.mainContent = document.querySelector("#content");
+    },
+
+    render() {
+        this.main = document.createElement("main");
+    },
+
+    appending() {
+        this.mainContent.appendChild(this.main);
+    },
+};
+
+App.init();
