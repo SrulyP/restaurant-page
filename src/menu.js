35d65f6cca50d201cc88f6c5b07fe9f3dda9b612
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-07-23 15:59:40 +0300

    Fill our skeleton code for the menu.js page

---
+++ src/menu.js
@@ -0,0 +1,36 @@
+const MenuPage = {
+    init() {
+        this.cacheDom();
+        this.render();
+        this.textFilling();
+        this.appending();
+    },
+
+    cacheDom() {
+        this.mainContent = document.querySelector("#content");
+    },
+
+    createEl(tag, className = null) {
+        const el = document.createElement(tag);
+        if (className) el.classList.add(className);
+        return el;
+    },
+
+    render() {
+        this.mainContent.innerHTML = "";
+        
+        this.main = this.createEl("main");
+    },
+
+    textFilling() {
+    },
+
+    appending() {
+        this.mainContent.appendChild(this.main);
+    },
+};
+
+export default function renderMenu() {
+    MenuPage.init();
+}
+
